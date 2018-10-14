affa8f341953524683661535d818c66b204cb8fa
Alex Sergeev <asergeev@uber.com>
2018-10-13 22:58:06 -0700

    Switch to MMCME2 for higher precision

--- src/main/scala/clock/Mmcme2Base.scala
+++ src/main/scala/clock/Mmcme2Base.scala
@@ -2,24 +2,24 @@ package clock
 
 import spinal.core._
 
-class Plle2Base(div0: Int = 1,
-                div1: Int = 1,
-                div2: Int = 1,
-                div3: Int = 1,
-                div4: Int = 1,
-                div5: Int = 1,
-                masterDiv: Int = 1,
-                masterMult: Int = 1,
-                clkInPeriod: Double) extends BlackBox {
+class Mmcme2Base(div0: Double = 1,
+                 div1: Int = 1,
+                 div2: Int = 1,
+                 div3: Int = 1,
+                 div4: Int = 1,
+                 div5: Int = 1,
+                 masterDiv: Int = 1,
+                 masterMult: Double = 1,
+                 clkInPeriod: Double) extends BlackBox {
   val generic = new Generic {
-    val CLKOUT0_DIVIDE = div0
+    val CLKOUT0_DIVIDE_F = div0
     val CLKOUT1_DIVIDE = div1
     val CLKOUT2_DIVIDE = div2
     val CLKOUT3_DIVIDE = div3
     val CLKOUT4_DIVIDE = div4
     val CLKOUT5_DIVIDE = div5
     val DIVCLK_DIVIDE = masterDiv
-    val CLKFBOUT_MULT = masterMult
+    val CLKFBOUT_MULT_F = masterMult
     val CLKIN1_PERIOD = clkInPeriod
   }
 
@@ -39,5 +39,5 @@ class Plle2Base(div0: Int = 1,
   }
 
   noIoPrefix()
-  setBlackBoxName("PLLE2_BASE")
+  setBlackBoxName("MMCME2_BASE")
 }
